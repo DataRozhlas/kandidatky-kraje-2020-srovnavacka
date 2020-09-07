e8868f79d79250d8bfddb70da830ce3342f832cd
Jan Boček <honza.bocek@gmail.com>
2020-09-07 16:47:29 +0200

    xxx

--- article.md
+++ article.md
@@ -9,33 +9,20 @@ options: [noheader, nopic]
 
 <wide><div id="kandidatky">
 
-<div id="stranyScroll"></div>
+<h3 id="pohled">Vyberte pohled</h3>
 
-<h3 id="kraj">Vyberte kraj</h3>
-
-<select id="krajSelect">
-  <option value="1">Středočeský</option>
-  <option value="2">Jihočeský</option>
-  <option value="3">Plzeňský</option>
-  <option value="4">Karlovarský</option>
-  <option value="5">Ústecký</option>
-  <option value="6">Liberecký</option>
-  <option value="7">Královéhradecký</option>
-  <option value="8">Pardubický</option>
-  <option value="9">Vysočina</option>
-  <option value="10">Jihomoravský</option>
-  <option value="11">Olomoucký</option>
-  <option value="12">Zlínský</option>
-  <option value="13">Moravskoslezský</option>
+<select id="pohledSelect">
+  <option value="kraj">Srovnání kandidátek v kraji</option>
+  <option value="strana">Srovnání kandidátek strany</option>
 </select>
 
-<h3>Kandidující strany</h3>
+<div id="secondLevel"></div>
 
-<div id="strany"></div>
-<div id="kandidatiScroll"></div>
-<div id="kandidati"></div>
+<div id="detail"></div>
 
 <div style="text-align: right;"><small>Zdroj dat: <a href="https://volby.cz/opendata/kz2020/kz2020_opendata.htm">volby.cz</a></small></div>
 
 </div></wide>
+
+výběr podle strany: teď není vidět do koalic
 
--- js/script.js
+++ js/script.js
@@ -2,57 +2,51 @@
 import Papa from "papaparse";
 
 // vyflusnuti tabulek
-function drawTables(stranyData, kandData, kraj) {
+function loadData(pohled, detail) {
+  Papa.parse("https://data.irozhlas.cz/kandidatky-kraje-2020-srovnavacka/data/srovnavacka.csv", {
+    download: true,
+    skipEmptyLines: true,
+    complete(results) {
+      drawDetail(results.data, pohled, detail);
+    }
+  });
+}
 
-  let stranyHlavickaData = stranyData[0];
+function drawDetail(data, pohled, detail) {
+  let hlavickaData = data[0];
+  let teloData;
+  if(pohled === 'kraj') {
+    data = data.filter(d => d[0] == detail);
+    hlavickaData.shift();
+    teloData = data.map(function(d) {return d.slice(1);});
+  } else if(pohled === 'strana') {
+    data = data.filter(d => d[1] == detail);
+//    hlavickaData = hlavickaData.slice(0, 1).concat(hlavickaData.slice(2));
+//    teloData = data.map(function(d) {return d.slice(0, 1).concat(d.slice(2))});
+    teloData = data.map(function(d) {return d});
+  }
 
-  // výběr kraje u stran
-  stranyData = stranyData.filter(d => d[0] == kraj);
+  console.log(hlavickaData);
+  console.log(teloData);
 
-  // po výběru kraje u stran odstranění prvního sloupce
-  stranyData = stranyData.map(function(d) { return d.slice(1);} );
-  stranyHlavickaData.shift();
+  $("#detail").html(`<table id="tabulka" class="display" style="width:100%"></table>`);
 
-  let kandHlavickaData = kandData[0];
+  const hlavicka = $("<tr>");
+  hlavickaData.forEach(column => hlavicka.append($("<th>").html(column)));
+  $("<thead>").appendTo("#tabulka").append(hlavicka);
 
-  // výběr kraje u kandidátů
-  kandData = kandData.filter(d => d[0] == kraj);
-
-  // po výběru kraje u kandidátů odstranění prvního sloupce
-  kandData = kandData.map(function(d) { return d.slice(1);} );
-  kandHlavickaData.shift();
-
-  $("#strany").html(`<table id="tabulkaStran" class="display" style="width:100%"></table>`);
-  $("#kandidati").html();
-
-  const stranyHlavicka = $("<tr>");
-  stranyHlavickaData.forEach(column => stranyHlavicka.append($("<th>").html(column)));
-  $("<thead>").appendTo("#tabulkaStran").append(stranyHlavicka);
-
-  // strany - telo (zbytek radku)
   const telo = $("<tbody>");
-  stranyData.forEach((strana) => {
+  teloData.forEach((strana) => {
     const radek = $("<tr>");
     strana.forEach((column, index) => {
-      index === 1
-        ? radek.append($("<td>").html(`${column} <div class="stranaKlik" data-strana=${strana[0]}><u>kandidáti</u></div>`))
-        : radek.append($("<td>").html(column));
+      radek.append($("<td>").html(column));
     });
-
-    // skryty sloupecek se jmeny kandidatu kvuli searchi
-    const kandidati = kandData
-      .filter(kand => kand[0] === parseInt(strana[0], 10).toString())
-      .map(kand => kand[2]);
-
-    radek.append($("<td>").html(kandidati.join(" ")));
-
     $(telo).append(radek);
   });
-  $("#tabulkaStran").append(telo);
+  $("#tabulka").append(telo);
 
-  // strany - datatablizace
-  $("#tabulkaStran").DataTable({
-    order: [[0, "asc"]],
+  $("#tabulka").DataTable({
+    order: [[1, "asc"]],
     responsive: true,
     ordering: true,
     paging: false,
@@ -61,92 +55,31 @@ function drawTables(stranyData, kandData, kraj) {
       url: "https://interaktivni.rozhlas.cz/tools/datatables/Czech.json",
     },
     columnDefs: [
-      { targets: 1, type: "diacritics-neutralise" },
-      { targets: 2, type: "diacritics-neutralise" },
-      { targets: 3, type: "natural" },
-      { targets: 7, visible: false, searchable: false }
+      { targets: 1, type: "diacritics-neutralise" }
     ],
   });
 
-  // onclick tabulka kandidatu
-  $(".stranaKlik").click((e) => {
-    const strana = e.currentTarget.getAttribute("data-strana");
-    const nazevStrany = stranyData.filter(str => str[0] === strana)[0][1];
-
-    $("#kandidati").html(`<button id="zpetStrany" type="button">Zpět na výběr strany</button>
-      <h3>Kandidáti</h3>
-      <h3 style="font-weight: normal">${nazevStrany}</h3>
-      <table id="tabulkaKandidatu" class="display" style="width:100%"></table>`);
-
-    // kandidati - hlavicka (prvni radek csv)
-    const kandHlavicka = $("<tr>");
-    kandHlavickaData.map(column => kandHlavicka.append($("<th>").html(column)));
-    $("<thead>").appendTo("#tabulkaKandidatu").append(kandHlavicka);
-
-    // kandidati - telo (filtr podle cisla strany)
-    const kandidati = kandData
-      .filter(kand => kand[0] == strana);
-
-    const kandTelo = $("<tbody>");
-    kandidati.forEach((kandidat) => {
-      const radek = $("<tr>");
-      kandidat.forEach(column => radek.append($("<td>").html(column)));
-      $(kandTelo).append(radek);
-    });
-    $("#tabulkaKandidatu").append(kandTelo);
-
-    // kandidati - datatablizace
-    $("#tabulkaKandidatu").DataTable({
-      columnDefs: [
-        { targets: 0, visible: false },
-        { targets: 2, type: "diacritics-neutralise" },
-        { targets: 4, type: "diacritics-neutralise" },
-        { targets: 5, type: "diacritics-neutralise" }
-      ],
-      order: [[0, "asc"]],
-      responsive: true,
-      ordering: true,
-      paging: false,
-      bInfo: false,
-      language: {
-        url: "https://interaktivni.rozhlas.cz/tools/datatables/Czech.json",
-      },
-    });
-
-    // scrolling od tlačítka a k němu
-    document.getElementById("kandidatiScroll").scrollIntoView();
-
-    $("#zpetStrany").click(() => {
-      document.getElementById("stranyScroll").scrollIntoView();
-    });
-  });
 }
 
-// nacteni tabulky kandidatu
-function loadKand(stranyData, kraj) {
-  Papa.parse("https://data.irozhlas.cz/kandidatky-kraje-2020/data/2020/app/kandidati.csv", {
-    download: true,
-    skipEmptyLines: true,
-    complete(results) {
-      drawTables(stranyData, results.data, kraj);
-    }
-  });
+function drawSecondLevel(pohled) {
+  if(pohled === 'kraj') {
+    let htmlSecondLevel = '<select id="secondLevelSelect"><option value="Středočeský">Středočeský</option><option value="Plzeňský">Plzeňský</option></select>';
+    $("#secondLevel").html(htmlSecondLevel);
+  } else if (pohled === 'strana') {
+    let htmlSecondLevel = '<select id="secondLevelSelect"><option value="ODS">ODS</option><option value="ČSSD">ČSSD</option></select>';
+    $("#secondLevel").html(htmlSecondLevel);
+  }
 }
 
-// nacteni tabulky stran
-function loadStrany(kraj) {
-  Papa.parse("https://data.irozhlas.cz/kandidatky-kraje-2020/data/2020/app/strany.csv", {
-    download: true,
-    skipEmptyLines: true,
-    complete(results) {
-      loadKand(results.data, kraj);
-    }
-  });
-}
-
-$('#krajSelect').change(function(event){
-  var kraj = $("#krajSelect").val();
-  loadStrany(kraj);
+$('#pohledSelect').change(function(event){
+  var pohled = $("#pohledSelect").val();
+  drawSecondLevel(pohled);
 });
 
-loadStrany(1);
+$('#secondLevel').change(function(event){
+  var pohled = $("#pohledSelect").val();
+  var detail = $("#secondLevelSelect").val();
+  loadData(pohled, detail);
+});
+
+// loadData(1);
